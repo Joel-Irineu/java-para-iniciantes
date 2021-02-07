e6c2d9275d7d65db290aec1e0a2d8a388f8fed1b
Joel Irineu <irineu.joel01@gmail.com>
2021-02-07 15:38:21 -0300

    teste tipos

---
+++ TesteTipos/src/main/java/com/mycompany/testetipos/TesteTipos.java
@@ -0,0 +1,9 @@
+package com.mycompany.testetipos;
+
+public class TesteTipos {
+    public static void main(String[] args) {
+        int idade = 30;
+        String valor = Integer.toString(idade);
+        System.out.println(valor);
+    }
+}
