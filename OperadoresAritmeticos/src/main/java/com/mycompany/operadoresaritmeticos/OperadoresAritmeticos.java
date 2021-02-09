ecb1c3e26d490a9ef55253b6413b61fedd93fc81
Joel Irineu <irineu.joel01@gmail.com>
2021-02-08 21:14:23 -0300

    operadores unarios e de atribuição

--- OperadoresAritmeticos/src/main/java/com/mycompany/operadoresaritmeticos/OperadoresAritmeticos.java
+++ OperadoresAritmeticos/src/main/java/com/mycompany/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -2,9 +2,22 @@ package com.mycompany.operadoresaritmeticos;
 
 public class OperadoresAritmeticos {
     public static void main(String[] args) {
-        int n1 = 3;
-        int n2 = 5;
-        float m = n1+n2/2;
-        System.out.println("A media é igual: " + m);
+        /** ARITIMETICOS
+         int n1 = 3;
+         int n2 = 5;
+         float m = n1+n2/2;
+         System.out.println("A media é igual: " + m);
+         **/
+        
+        /** UNARIOS 
+         int numero = 10;
+         int valor = 4 + numero--;
+         System.out.println(valor);
+         System.out.println(numero);
+         */
+        
+        int x = 4;
+        x *= 2;
+        System.out.println(x);
     }
 }
